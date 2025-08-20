b60055cce7c6ac324025ce7201c29429d8e81e9f
Ken Corbett <ken@truepic.com>
2025-08-20 14:37:26 -0400

    Removing dist files

--- dist/errors/not_implemented.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const base_1 = __importDefault(require("./base"));
-class NotImplementedError extends base_1.default {
-}
-exports.default = NotImplementedError;
