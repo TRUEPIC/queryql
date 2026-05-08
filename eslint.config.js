214ae34a8af5fa6b82b44fbe7966a9c46f8c3491
Justin Stayton <jstayton@users.noreply.github.com>
2026-05-08 08:44:38 -0400

    Update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 const js = require('@eslint/js')
 const prettier = require('eslint-config-prettier')
 const jest = require('eslint-plugin-jest')
-const node = require('eslint-plugin-n')
+const node = require('eslint-plugin-n').default
 const globals = require('globals')
 
 module.exports = [
